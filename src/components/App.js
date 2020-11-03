ced79d27a5c7b4ecd9635222993d72bb2c33243c
Meissam Rasouli <meissam.rasouli@gmail.com>
2020-11-03 20:20:58 +0330

    remove redundat files and add bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+
+
+export default class App extends Component {
+    render() {
+
+        return (
+            <>
+                <h1>Meissm</h1>
+            </>
+        )
+    }
+}
