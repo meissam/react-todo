b1c939a14df5f49f7308848ead9e1c1e890a67a7
Meissam Rasouli <meissam.rasouli@gmail.com>
2020-11-29 00:15:38 +0330

    sort by items by date

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import Item from './Item';
-
+import React from "react";
+import Item from "./Item";
+import dayjs from "dayjs";
 
 const Items = (props) => {
-    const {items, handleRemoveItem} = props
-    return(
-        <ul className="todo-list list-unstyled">
-            {items.map((item) => (
-               <Item key={item.id} data={item} handleRemoveItem={handleRemoveItem} />
-            ))}
-          </ul>
-    )
-
-}
+  const { items, handleRemoveItem } = props;
+  return (
+    <ul className="todo-list list-unstyled">
+      {items
+        .sort(function (a, b) {
+          const date1 = dayjs(b.lastModified);
+          const date2 = dayjs(a.lastModified);
+          return date1.diff(date2);
+        })
+        .map((item) => (
+          <Item key={item.id} data={item} handleRemoveItem={handleRemoveItem} />
+        ))}
+    </ul>
+  );
+};
 
 export default Items;
